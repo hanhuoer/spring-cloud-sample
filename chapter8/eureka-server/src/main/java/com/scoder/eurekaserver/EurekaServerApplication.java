f556e610bced5ca9aee375e47fdefe9cb58ab1b9
shaokang <17739393975@163.com>
2019-08-28 18:02:59 +0800

    chapter8 - 消息总线 Spring Cloud Bus

---
+++ chapter8/eureka-server/src/main/java/com/scoder/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.scoder.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 创建服务注册中心
+ * EnableEurekaServer 通过这个注解启动一个服务注册中心
+ *
+ * @author shaokang
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
