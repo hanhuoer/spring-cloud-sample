fe573601401c62c6625951717c3f5c2a5e2a99e7
shaokang <17739393975@163.com>
2019-08-28 16:37:14 +0800

    chapter7 - 分布式高可用配置中心 Spring Cloud Config

---
+++ chapter7/config-server/src/main/java/com/scoder/configserver/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.scoder.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * EnableConfigServer 开启配置服务器
+ *
+ * @author shaokang
+ **/
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
